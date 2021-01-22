e81b9249042b8487a4d3e9dd9dc72225946aefbf
Brett Edwards <Brett.1.Edwards@gov.bc.ca>
2021-01-22 15:57:42 -0800

    Updated Config_Sample to reflect current config; added __init__ method to enable choosing Test or Prod upon initialization

--- TFL_Updates/TFL_Config_Sample.py
+++ TFL_Updates/TFL_Config_Sample.py
@@ -5,37 +5,40 @@
 ## Date: February, 2020
 ## Author: Jed Harrison, GeoBC
 ###############################################################################
+## Edited: January 2021
+## By: Brett Edwards, GeoBC
+## Change: Add __init__ function, giving the user/code the ability to run in either test or prod upon initializing TFL_Path()
 
-#MOST IMPORTANT: Set this one variable to true/false when switching between test and production tools
-#TEST - Checking to determine Github desktop diff behaviour when using delete/copy from other folder
-test = True
 
 class Resources():
-    GEOBC_LIBRARY_PATH = R'\\UNC\path\to\GeoBC\library'
+    GEOBC_LIBRARY_PATH = R'\\spatialfiles.bcgov\ilmb\dss\dsswhse\Tools and Resources\Scripts\Python\Library'
 
 class TFL_Path():
     """Class of properties for file paths. Uses test boolean to determine if
        path properties point to test or production data folders. Boolean is
-       hard-coded in TFL_Config.py, not passed from calling script"""
+    passed from calling script, with a default setting of working in test"""
+    def __init__(self, test = True):
+        self.test = test
 
-    if test:
-        FINAL_FOLDER = R'\\UNC\path\to\test\1_TFL_Final'
-        EDITS_FOLDER = R'\\UNC\path\to\test\2_TFL_Working'
-        REVIEW_FOLDERS = R'\\UNC\path\to\test\3_TFL_Review'
-        PENDING_FOLDERS = R'\\UNC\path\to\test\4_TFL_Pending'
-        ARCHIVE = R'\\UNC\path\to\test\5_TFL_Archive'
-        STAGING = R'\\UNC\path\to\test\TFL_Staging'
-        TEMPLATES_GDB = R'\\UNC\path\to\test\TFL_templates\data\FADM_TFL_XX.gdb\TFL_Data'
-        TEMPLATE_MAP = R'\\UNC\path\to\test\TFL_templates\arcgisprojects\TFL_Review_Edits_template.mxd'
-    else:
-        FINAL_FOLDER = R'\\UNC\path\to\prod\1_TFL_Final'
-        EDITS_FOLDER = R'\\UNC\path\to\prod\2_TFL_Working'
-        REVIEW_FOLDERS = R'\\UNC\path\to\prod\3_TFL_Review'
-        PENDING_FOLDERS = R'\\UNC\path\to\prod\4_TFL_Pending'
-        ARCHIVE = R'\\UNC\path\to\prod\5_TFL_Archive'
-        STAGING = R'\\UNC\path\to\prod\TFL_Staging\data'
-        TEMPLATES_GDB = R'\\\UNC\path\to\prod\TFL_templates\data\FADM_TFL_XX.gdb\TFL_Data'
-        TEMPLATE_MAP = R'\\UNC\path\to\prod\TFL_templates\arcgisprojects\TFL_Review_Edits_template.mxd'
+        if self.test:
+            self.FINAL_FOLDER = R'\\UNC\path\to\test\1_TFL_Final'
+            self.EDITS_FOLDER = R'\\UNC\path\to\test\2_TFL_Working'
+            self.REVIEW_FOLDERS = R'\\UNC\path\to\test\3_TFL_Review'
+            self.PENDING_FOLDERS = R'\\UNC\path\to\test\4_TFL_Pending'
+            self.ARCHIVE = R'\\UNC\path\to\test\5_TFL_Archive'
+            self.STAGING = R'\\UNC\path\to\test\TFL_Staging'
+            self.TEMPLATES_GDB = R'\\UNC\path\to\test\TFL_templates\data\FADM_TFL_XX.gdb\TFL_Data'
+            self.TEMPLATE_MAP = R'\\UNC\path\to\test\TFL_templates\arcgisprojects\TFL_Review_Edits_template.mxd'
+        else:
+            self.FINAL_FOLDER = R'\\UNC\path\to\prod\1_TFL_Final'
+            self.EDITS_FOLDER = R'\\UNC\path\to\prod\2_TFL_Working'
+            self.REVIEW_FOLDERS = R'\\UNC\path\to\prod\3_TFL_Review'
+            self.PENDING_FOLDERS = R'\\UNC\path\to\prod\4_TFL_Pending'
+            self.ARCHIVE = R'\\UNC\path\to\prod\5_TFL_Archive'
+            self.STAGING = R'\\UNC\path\to\prod\TFL_Staging\data'
+            self.TEMPLATES_GDB = R'\\\UNC\path\to\prod\TFL_templates\data\FADM_TFL_XX.gdb\TFL_Data'
+            self.TEMPLATE_MAP = R'\\UNC\path\to\prod\TFL_templates\arcgisprojects\TFL_Review_Edits_template.mxd'
+
 
 if __name__ == '__main__':
     pass
